8c930224bf3429b10ef5eed2f7c1214ea7b49432
Faaiq Ali Khan <50907582+FaaiqKhan@users.noreply.github.com>
2023-11-17 17:52:28 +0100

    room dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,15 @@ android {
         addConstant("S3_ACCESS_KEY", "\"AKIA6JAJNGO6KI2BGD2P\"")
         addConstant("S3_SECRET_KEY", "\"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\"")
         addConstant("S3_BUCKET_NAME", "\"aipoctw\"")
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments += mapOf(
+                    "room.schemaLocation" to "$projectDir/schemas",
+                    "room.incremental" to "true"
+                )
+            }
+        }
     }
     buildTypes {
         release {
@@ -100,15 +109,18 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.media3:media3-exoplayer:1.1.1")
-    implementation("androidx.media3:media3-exoplayer-dash:1.1.1")
-    implementation("androidx.media3:media3-ui:1.1.1")
+    // Exo player
+    val exo_player = "1.2.0"
+    implementation("androidx.media3:media3-exoplayer:$exo_player")
+    implementation("androidx.media3:media3-exoplayer-dash:$exo_player")
+    implementation("androidx.media3:media3-ui:$exo_player")
+
     implementation("androidx.compose.ui:ui-viewbinding:1.5.4")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.dagger:hilt-android:2.48")
@@ -128,7 +140,16 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:1.2.0")
     implementation("io.arrow-kt:arrow-fx-coroutines:1.2.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
+    // Database
+    val room_version = "2.6.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+
     testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.room:room-testing:$room_version")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
